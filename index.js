d33e136e57ee764e819c2ba8e7ca9b94d3269bd7
eniallator <eniallatormc@gmail.com>
2019-05-30 20:54:27 +0100

    Made a working version without the line collision detection

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+const minSpawnTime = 120
+const spawnRandomness = 40
+
+const lines = [new Line(canvas.width / 2, canvas.height, (3 / 2) * Math.PI)]
+
+ctx.lineWidth = 5
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'white'
+
 function run() {
-    ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    for (let line of lines) {
+        line.update({ x: canvas.width, y: canvas.height }, lines)
+        line.draw(ctx)
+    }
 
     requestAnimationFrame(run)
 }
